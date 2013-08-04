33a01d48e81537f5e3ee98d61bdc2dd01449647b
Emmett Butler <emmett.butler321@gmail.com>
2013-08-04 18:04:48 -0400

    increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Candela',
-      version='0.1.1',
+      version='0.2.0',
       description="Python shell creation library",
       author="Emmett Butler",
       author_email="emmett.butler321@gmail.com",
